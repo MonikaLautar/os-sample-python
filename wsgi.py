182b2cd8209a2e92e6a78541067f64533755df9a
MonikaLautar <33925181+MonikaLautar@users.noreply.github.com>
2021-03-26 15:07:51 +0100

    Update wsgi.py
    Brisala sem tekst iz pozdrava.

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World! Kako ste danes?"
+    return "Hello World!"
 
 if __name__ == "__main__":
     application.run()
