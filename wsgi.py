5b1b18421534f2a764351b0b0ef5349fa8ea1d09
MonikaLautar <33925181+MonikaLautar@users.noreply.github.com>
2021-03-26 15:20:03 +0100

    Update wsgi.py
    Dodali smo v pozdrav

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World! Nekaj moramo dodati..."
 
 if __name__ == "__main__":
     application.run()
